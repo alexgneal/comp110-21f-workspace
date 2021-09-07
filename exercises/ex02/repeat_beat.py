a08cb1d782193ccbfac338bc7fd2d9f1fee16268
Alexandra Neal <alexneal@live.unc.edu>
2021-09-07 16:03:31 -0400

    Fixed repeat beat

--- exercises/ex02/repeat_beat.py
+++ exercises/ex02/repeat_beat.py
@@ -9,10 +9,11 @@ user_beat: str = input("What beat do you want to repeat? ")
 repeat: int = int(input("How many times do you want to repeat it? "))
 
 i: int = 0
-beat: str = ""
+beat: str = user_beat
+
 if repeat > 0:
-    while i < repeat:
-        beat = ((user_beat + " ") * repeat)
+    while i < repeat - 1:
+        beat = beat + " " + user_beat
         i = i + 1
     print(beat)
 else:
