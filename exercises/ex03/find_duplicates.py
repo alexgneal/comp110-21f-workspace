93646195af10d3d92a9ac8858b00d7f22fc79d75
Alexandra Neal <alexneal@live.unc.edu>
2021-09-14 14:08:01 -0400

    finished EX03

--- exercises/ex03/find_duplicates.py
+++ exercises/ex03/find_duplicates.py
@@ -1,3 +1,19 @@
 """Finding duplicate letters in a word."""
 
-__author__ = "123456789"
+__author__ = "730332719"
+
+word: str = input("Enter a word: ")
+duplicate: bool = False
+
+i: int = 0
+while i < len(word):
+    char = word[i]
+    j: int = 0
+    while j < len(word):
+        if word[j] == char:
+            duplicate = True
+        else: 
+            duplicate = False
+        j += 1
+    i += 1
+print("Found duplicate: " + str(duplicate))
--- exercises/ex03/happy_trees.py
+++ exercises/ex03/happy_trees.py
@@ -1,6 +1,17 @@
 """Drawing forests in a loop."""
 
-__author__ = "123456789"
+__author__ = "730332719"
 
 # The string constant for the pine tree emoji
 TREE: str = '\U0001F332'
+
+depth: int = int(input("Depth: "))
+
+i: int = 0
+tree = TREE
+
+if depth > 0:
+    while i < depth:
+        tree = TREE * (i + 1)
+        i = i + 1
+        print(tree)
